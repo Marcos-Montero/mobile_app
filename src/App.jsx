0aebcc9f2df2ac205e7a6d1e680cec45201d5220
MSI Melmoth <music.melmoth@gmail.com>
2022-03-04 22:50:56 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <main>
+        <h1>Tik Tok Clon</h1>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
